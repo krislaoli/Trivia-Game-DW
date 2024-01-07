4e9b9ca0c97cb51c5fe553b2eedd404db1ec698f
Pak Coding <laolikristian1305@gmail.com>
2024-01-07 11:27:26 +0700

    First Commit

---
+++ App.tsx
@@ -0,0 +1,51 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import LandingPage from "./pages/LandingPage"
+import "react-native-gesture-handler"
+import Profile from "./pages/Profile"
+import SplashScreen from "./pages/SplashScreen"
+import StartGame from "./pages/StartGame"
+
+const Stack = createStackNavigator()
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="StartGame">
+          <Stack.Screen
+            name="SplashScreen"
+            component={Profile}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="LandingPage"
+            component={LandingPage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="StartGame"
+            component={StartGame}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
+export default App
